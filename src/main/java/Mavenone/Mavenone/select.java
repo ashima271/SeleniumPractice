cbe5978a218fb5209b5a89adee21a0e80ae23b9d
Ashima <ashimajindal169@gmail.com>
2025-11-13 00:33:51 +0530

    added new select option

--- src/main/java/Mavenone/Mavenone/select.java
+++ src/main/java/Mavenone/Mavenone/select.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class practice_day2 {
+public class select {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
@@ -16,17 +16,15 @@ public class practice_day2 {
 		js.executeScript("window.scrollBy(0,500)");
 		
 		driver.findElement(By.xpath("//*[text()='Elements']")).click();
-		driver.findElement(By.xpath("//*[text()='Text Box']")).click();
-		driver.findElement(By.id("userName")).sendKeys("Ragon");
-		driver.findElement(By.id("userEmail")).sendKeys("ragon@yopmail.com");
+		driver.findElement(By.xpath("//*[text()='Radio Button']")).click();
 		
 		JavascriptExecutor js1 = (JavascriptExecutor) driver;
 		js1.executeScript("window.scrollBy(0,500)");
-		
-		driver.findElement(By.id("submit")).click();		
-		
+		driver.findElement(By.xpath("//label[text()='Yes']")).click();
 		
 		
-	}
 
+	}
+	
+	
 }
